189277af80d83772cb062d04b98ae7762ec76c67
TheStankela <ognjenogistankovic2002@gmail.com>
2023-05-12 13:41:08 +0200

    Configured db context and dependencies for identity framework

---
+++ UsluzniOglasi/UsluzniOglasi.Infrastructure/Context/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsluzniOglasi.Domain.Models;
+
+namespace UsluzniOglasi.Infrastructure.Context
+{
+    public class UserDbContext : IdentityDbContext<AppUser>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
