ba7df0cf0d980d872329c074e3c0d25cec38eeab
TheStankela <ognjenogistankovic2002@gmail.com>
2023-05-12 15:28:32 +0200

    Added AuthController for user registration and login

---
+++ UsluzniOglasi/UsluzniOglasi.API/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UsluzniOglasi.Application.Models;
+using UsluzniOglasi.Application.Services;
+
+namespace UsluzniOglasi.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginModel userLoginModel)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!await _authService.Login(userLoginModel))
+                return BadRequest(ModelState);
+
+            return Ok("Logged in successfully!");
+        }
+        [AllowAnonymous]
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserRegisterModel userRegisterModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!await _authService.Register(userRegisterModel))
+                return BadRequest(ModelState);
+
+            return Ok("Registered successfully!");
+        }
+        [Authorize]
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Register()
+        {
+            if (!await _authService.Logout())
+                return BadRequest(ModelState);
+
+            return Ok("Logged out successfully!");
+        }
+    }
+}
