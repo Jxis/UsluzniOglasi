595c6ab5f7b8d0291817cb2811b50ff25e99cf32
TheStankela <ognjenogistankovic2002@gmail.com>
2023-05-12 13:40:46 +0200

    Added models for inital configuration for identity

---
+++ UsluzniOglasi/UsluzniOglasi.Domain/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsluzniOglasi.Domain.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ProfilePictureURL { get; set; }
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+    }
+}
