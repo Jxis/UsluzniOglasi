aa9f20c2e5201b7a16c0cec5af86211fcede76f6
TheStankela <ognjenogistankovic2002@gmail.com>
2023-05-13 16:59:20 +0200

    Added UserImageService for uploading images to Amazon S3 Bucket

---
+++ UsluzniOglasi/UsluzniOglasi.Application/Services/IUserImageService.cs
@@ -0,0 +1,10 @@
+﻿using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+
+namespace UsluzniOglasi.Application.Services
+{
+    public interface IUserImageService
+    {
+        Task<PutObjectResponse> UploadImageAsync(string id, IFormFile file);
+    }
+}
---
+++ UsluzniOglasi/UsluzniOglasi.Application/Services/UserImageService.cs
@@ -0,0 +1,42 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsluzniOglasi.Application.Services
+{
+    public class UserImageService : IUserImageService
+    {
+        private readonly IAmazonS3 _s3;
+        private readonly IConfiguration _configuration;
+
+        public UserImageService(IAmazonS3 s3, IConfiguration configuration)
+        {
+            _s3 = s3;
+            _configuration = configuration;
+        }
+        public async Task<PutObjectResponse> UploadImageAsync(string id, IFormFile file)
+        {
+            var putObjectRequest = new PutObjectRequest
+            {
+                BucketName = _configuration.GetSection("AWS:BucketName").Value,
+                Key = $"profile_images/{id}",
+                ContentType = file.ContentType,
+                InputStream = file.OpenReadStream(),
+                Metadata =
+                {
+                    ["x-amz-meta-originalname"] = file.FileName,
+                    ["x-amz-meta-extension"] = Path.GetExtension(file.FileName)
+                }
+            };
+            return await _s3.PutObjectAsync(putObjectRequest);
+        }
+    }
+}
